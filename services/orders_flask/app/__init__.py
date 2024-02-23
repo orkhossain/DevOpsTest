781b1b4b81177dd30228ce150a00db05ea29e5ef
Ork Hossain Muntaqin <orkhoss.2011@gmail.com>
2024-02-23 19:28:24 +0100

    feat(flask): multi threading

--- services/orders_flask/app/__init__.py
+++ services/orders_flask/app/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import os
+from threading import Thread
+from app.workers import rabbitmq_worker
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://user:password@postgres:5432/orders'
@@ -7,8 +10,12 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-# Import routes to register them with the Flask app
-from app.routes import order_routes
+from app.routes import order_routes 
 
-# Import workers to run them with the Flask app
-from app.workers import rabbitmq_worker
+rabbitmq_thread = Thread(target=rabbitmq_worker.rabbitmq_worker)
+rabbitmq_thread.daemon = True
+rabbitmq_thread.start()
+
+
+if __name__ == '__main__':
+    app.run(debug=True,threaded=True,processes=2, host='0.0.0.0', port=5000)
