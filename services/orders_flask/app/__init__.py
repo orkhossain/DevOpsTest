a76e2d156a805dada7ef9220a3939dec0a4fc8c3
Ork Hossain Muntaqin <orkhoss.2011@gmail.com>
2024-02-23 20:05:18 +0100

    feat(flask): add kafka integration

--- services/orders_flask/app/__init__.py
+++ services/orders_flask/app/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 from threading import Thread
 from app.workers import rabbitmq_worker
+from app.workers import kafka_worker
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://user:password@postgres:5432/orders'
@@ -16,6 +17,9 @@ rabbitmq_thread = Thread(target=rabbitmq_worker.rabbitmq_worker)
 rabbitmq_thread.daemon = True
 rabbitmq_thread.start()
 
+kafka_thread = Thread(target=kafka_worker.kafka_consumer)
+kafka_thread.daemon = True
+kafka_thread.start()
 
 if __name__ == '__main__':
-    app.run(debug=True,threaded=True,processes=2, host='0.0.0.0', port=5000)
+    app.run(debug=True,threaded=True,processes=3, host='0.0.0.0', port=5000)
