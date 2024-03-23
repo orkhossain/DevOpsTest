679f72a867871c38946aeac09949779a4e49e1b2
Ork Hossain Muntaqin <orkhoss.2011@gmail.com>
2024-03-23 22:38:33 +0100

    feat(mongo): add required

--- services/users_dotnet/Controllers/MongoDBSettings.cs
+++ services/users_dotnet/Controllers/MongoDBSettings.cs
@@ -1,5 +1,5 @@
 public class MongoDBSettings
 {
-    public string ConnectionString { get; set; }
-    public string DatabaseName { get; set; }
+    public required string ConnectionString { get; set; }
+    public required string DatabaseName { get; set; }
 }
